4279efd8f854e465cf4c569698e8363e4a219131
zhitern <tangzhitern@gmail.com>
2023-04-02 20:13:05 +0800

    Updated Navbar's buttons to link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/AlgoShowcasePage">Geo Topic Modeling</a>
-                <a href="/RegionSearchPage">Region Search</a>
-                <a href="/AlgoShowcasePage">Region Topic Exploration</a>
-                <a href="/DeepRegionRepresentationPage">Deep Region Representation</a>
-                <a href="https://personal.ntu.edu.sg/gaocong/projectS.html" target="_blank">Related Reseach Projects</a>
+                <Link to="/">Home</Link>
+                <Link to="/AlgoShowcasePage">Geo Topic Modeling</Link>
+                <Link to="/RegionSearchPage">Region Search</Link>
+                <Link to="/AlgoShowcasePage">Region Topic Exploration</Link>
+                <Link to="/DeepRegionRepresentationPage">Deep Region Representation</Link>
+                <Link to="https://personal.ntu.edu.sg/gaocong/projectS.html" target="_blank">Related Reseach Projects</Link>
             </div>
         </nav>
     );
